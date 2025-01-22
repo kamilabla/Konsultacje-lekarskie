a4bc0212511883a9308195ef7894fd21f77f3f76
kamilabla <kambla@student.agh.edu.pl>
2025-01-22 21:27:27 +0100

    styl

--- lekarz-kalendarz/src/app/calendar/calendar.component.ts
+++ lekarz-kalendarz/src/app/calendar/calendar.component.ts
@@ -59,6 +59,7 @@ export class CalendarComponent {
     const week = [];
     const today = new Date();
 
+
     for (let i = 0; i < 7; i++) {
       const date = new Date();
       date.setDate(today.getDate() - today.getDay() + i);
